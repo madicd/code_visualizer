a61d47f320186eaf37293c62d26dfddb648bfa71
Djordje Madic <djordje.madic@zuehlke.com>
2024-09-20 22:27:20 +0200

    Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- Make it available as executable using `dart pub global activate`.
+
 ## 1.0.0
 
 - Initial version.
--- test/code_visualizer_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:code_visualizer/code_visualizer.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
